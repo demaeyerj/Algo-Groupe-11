aad9a80fe1cd0b52e54c4d8172e99fd2cb4de4e1
demaeyerj <julien.demaeyer@gmail.com>
2013-09-29 14:27:39 +0200

    pop,dup,exch.
    Vérification de pile vide à ajouter dans les autres méthodes ? add,
    sub,...

--- Missions/src/mission/one/PostScript.java
+++ Missions/src/mission/one/PostScript.java
@@ -50,7 +50,49 @@ public class PostScript {
 		double temp = (double) s.pop() / (double) s.pop();
 		s.push(temp);
 	}
-
+	
+	/**
+	 * 
+	 * @post Test si les deux premiers elements de la pile sont egaux
+	 */
+	public void eq(Stack s){
+		boolean temp = (double) s.pop()==(double) s.pop();
+	}
+	
+	/**
+	 * @pre 
+	 * @post Echange la place des deux elements top de la stack
+	 */
+	public void exch(Stack s) {
+		if(s.size()>2){
+		double temp1= (double) s.pop();
+		double temp2= (double) s.pop();
+		s.push(temp1);
+		s.push(temp2);
+		}
+	}
+	
+	/**
+	 * 
+	 * @post push une copie du "top element" de la stock au somment de celle-ci
+	 */
+	public void dup(Stack s) {
+		if(s.isEmpty()!=true){
+			double temp= (double) s.peek();
+			s.push(temp);
+		}
+	}
+	
+	/**
+	 * @pre 
+	 * @post retire le "top element" de la stack
+	 */
+	public void pop(Stack s){
+		if(s.isEmpty()!=true){
+			s.pop();
+		}
+	}
+	
 	/**
 	 * @pre: argument ligne non null
 	 * @post: decode et execute les instructions presentes dans le String ligne.
